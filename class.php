b45808f87e50f3899a079221dbc404101b664d98
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-23 14:11:18 +0200

    init

---
+++ class.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche: - L’e-commerce vende 
+
+ **prodotti** per animali.
+ - I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
+ - I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
+
+  Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+*/
+class Product {
+  
+};
+
+class Category extends Product {
+
+};
+
+
+class Food extends Category {
+
+};
+
+class Game extends Category {
+
+};
+
+class Sit extends Category {
+
+};
+
+
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+include_once __DIR__ . "/server.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <title>Document</title>
+</head>
+<body>
+  <div class="mainwrapper">
+    <div class="container mt-5">
+
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>s
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
